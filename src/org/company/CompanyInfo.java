e9494eaf3942b1933a01db1b0a95150eaaf4890e
alwindanny <alwindannymnr@gmail.com>
2023-12-17 22:15:11 +0530

    functional test1

---
+++ src/org/company/CompanyInfo.java
@@ -0,0 +1,35 @@
+package org.company;
+
+public class CompanyInfo {
+
+	public void companyName() {
+
+		System.out.println("Infosys");
+	}
+
+	public void companyName(String name) {
+
+		System.out.println(name);
+	}
+
+	public void companyName(int companycode) {
+		System.out.println(companycode);
+
+	}
+
+	public void companyName(String name, int companycode) {
+
+		System.out.println("code:" + companycode + '\n' + "name:" + name);
+
+	}
+
+	public static void main(String[] args) {
+
+		CompanyInfo in = new CompanyInfo();
+		in.companyName();
+		in.companyName("TCS");
+		in.companyName(123);
+		in.companyName("HCL", 1005);
+	}
+
+}
